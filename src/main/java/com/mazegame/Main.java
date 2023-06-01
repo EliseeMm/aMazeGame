d0a58736728e0acf23195abd92a90e2cd99bf1f9
elmathe022 <elmathe022@student.wethinkcode.co.za>
2023-06-01 17:01:26 +0200

    moved runner to separate class to neaten code

--- src/main/java/com/mazegame/Main.java
+++ src/main/java/com/mazegame/Main.java
@@ -5,17 +5,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
-import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -107,40 +103,12 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
-    public class Runner extends Rectangle {
-        private int startingX;
-        private int startingY;
 
-        private Runner(int startingX,int startingY,Color color,int obstacleSize){
-            super(startingX,startingY,color);
-            setWidth((double) obstacleSize /3);
-            setHeight((double) obstacleSize /3);
-            setTranslateX(startingX);
-            setTranslateY(startingY);
-        }
-
-        void moveUp(){
-            setTranslateY(getTranslateY() - 100);
-        }
-        void moveDown(){
-            setTranslateY(getTranslateY() + 100);
-        }
-        void moveRight(){
-            setTranslateX(getTranslateX() + 100);
-        }
-        void moveLeft(){
-            setTranslateX(getTranslateX() - 100);
-        }
-
-
-    }
 
     public Cell getRunnerStartingCell(ArrayList<ArrayList<Cell>> grid){
         Random random = new Random();
 
         int rowIndex = random.nextInt(grid.size());
-        System.out.println(grid.size());
-        System.out.println(rowIndex);
 
         ArrayList<Cell> row = grid.get(rowIndex);
 
---
+++ src/main/java/com/mazegame/Runner.java
@@ -0,0 +1,29 @@
+package com.mazegame;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Runner extends Rectangle {
+    protected Runner(int startingX, int startingY, Color color, int obstacleSize){
+        super(startingX,startingY,color);
+        setWidth((double) obstacleSize /3);
+        setHeight((double) obstacleSize /3);
+        setTranslateX(startingX);
+        setTranslateY(startingY);
+    }
+
+    void moveUp(){
+        setTranslateY(getTranslateY() - 100);
+    }
+    void moveDown(){
+        setTranslateY(getTranslateY() + 100);
+    }
+    void moveRight(){
+        setTranslateX(getTranslateX() + 100);
+    }
+    void moveLeft(){
+        setTranslateX(getTranslateX() - 100);
+    }
+
+
+}
