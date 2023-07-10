c7bf43b3cb6a1860b09e99c853a6ac60e224a6f1
elmathe022 <elmathe022@student.wethinkcode.co.za>
2023-07-10 16:30:28 +0200

    The player info classes captures the name of the player as well as their score which will then be written to a database

---
+++ src/main/java/com/mazegame/PlayerInfo.java
@@ -0,0 +1,28 @@
+package com.mazegame;
+
+public class PlayerInfo {
+    private static String playerName;
+    private static int playerScore;
+
+    public static String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        PlayerInfo.playerName = playerName;
+    }
+
+    public static int getPlayerScore() {
+        return playerScore;
+    }
+
+    public static void setPlayerScore(int playerScore) {
+        PlayerInfo.playerScore = playerScore;
+    }
+
+    @Override
+    public String toString(){
+        return playerName;
+    }
+
+}
